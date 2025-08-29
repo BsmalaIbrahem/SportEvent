708f9e06d3045af2ad5c8d5a5b969424ba0c5637
Youssef Saied <di0ab0lo0@gmail.com>
2025-08-29 21:31:43 +0300

    Done For Contact

--- PresentationLayer/Areas/Customer/Controllers/ContactController.cs
+++ PresentationLayer/Areas/Customer/Controllers/ContactController.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+using DataAccessLayer.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace PresentationLayer.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class ContactController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ContactController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new Contact());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(Contact contact)
+        {
+            if(!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+
+            contact.IsRead = false;
+
+            await _unitOfWork.ContactRepository.AddAsync(contact);
+            await _unitOfWork.ContactRepository.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Message Sent Succsesfully";
+
+            return RedirectToAction("Index");
         }
     }
 }
