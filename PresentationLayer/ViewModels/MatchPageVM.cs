2017c9f3da0a99525f3644e73a8beae2a7bc76be
Youssef Saied <di0ab0lo0@gmail.com>
2025-08-30 19:55:40 +0300

    Match Controller update 30-08-2025

--- PresentationLayer/ViewModels/MatchPageVM.cs
+++ PresentationLayer/ViewModels/MatchPageVM.cs
@@ -6,6 +6,7 @@ namespace PresentationLayer.ViewModels
     {
         public Match CurrentMatch { get; set; }
         public List<TeamPlayer> TeamPlayer { get; set; }
+        public List<MatchStatistic> Goals { get; set; } = new();
         public Match NextMatch { get; set; }
         public List<Match> UpcomingMatches { get; set; }
     }
