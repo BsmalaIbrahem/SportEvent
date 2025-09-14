ffbe18f4f8cf400be3698ac9c256e657ed4b0a2b
Basmala Ibrahim <102371084+BsmalaIbrahem@users.noreply.github.com>
2025-09-14 22:12:13 +0300

    Home Page

--- PresentationLayer/ViewModels/CustomerHomeVM.cs
+++ PresentationLayer/ViewModels/CustomerHomeVM.cs
@@ -1,7 +1,12 @@
-﻿namespace PresentationLayer.ViewModels
+﻿using DataAccessLayer.Models;
+
+namespace PresentationLayer.ViewModels
 {
     public class CustomerHomeVM
     {
         public List<LeagueStandingVM> LeagueStandings { get; set; } = new List<LeagueStandingVM>();
+        public Match? NextMatch { get; set; }
+        public IEnumerable<New>? Blogs { get; set; }
+        public IEnumerable<Match>? UpcommingMatches { get; set; }
     }
 }
